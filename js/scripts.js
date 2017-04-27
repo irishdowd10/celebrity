1c2036cf5d9b73295002c1632e2c5e02483f630a
Anabel Ramirez and john dowd <anabel@>
2017-04-27 16:22:49 -0700

    changes if else values needs hide still.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,30 @@
 $(document).ready(function() {
   $(".formOne").submit(function(event) {
+  var nameInput = $("input#name").val();
   var planet = parseInt($("select#planet").val());
   var gender = parseInt($("select#gender").val());
   var color =  parseInt($("select#color").val());
   var result = planet + gender + color;
 
-  if (result < 4) {
+  if (result<= 4) {
     $("#servo").show();
     }
-  //  /else if (age >=18 && gender === "female"  && color === "blue" && "red"  && "green") {
-  //     $("#gypsy").show();
-  //   }
-  //   else if (age >=18 /*&& age <=100*/ && gender === "robot"  /*&& color === "blue" && "red"  && "green"*/) {
-  //     $("#crow").show();
-  //   }
-  //   else if (age <18) {
-  //     $("#no-robots").show();
-  //   }
+   else if (result<=6) {
+      $("#gypsy").show();
+    }
+    else if (result <=8) {
+      $("#crow").show();
+    }
+    else {
+      $("#no-robots").show();
+    }
 
-
-  event.preventDefault();
+  $(".name").text(nameInput);
+    event.preventDefault();
   });
+
+  // $("div.answers").children(p).first().submit(function() {
+  //   $(this).remove();
+  // });
 
 });
