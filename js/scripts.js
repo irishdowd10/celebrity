56b9ef2f9dfe02395818ebdeec9c8a70712351a7
Anabel Ramirez and john dowd <john@>
2017-04-27 13:33:48 -0700

    adds branch javascript for choosing robots.

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,23 @@ $(document).ready(function() {
   var gender = $("select#gender").val();
   var color =  $("select#color").val();
 
-  if (age >= 18 && age <=26 && gender === "male"  && color === "blue" && "red") {
+  if (age >=18 && gender === "male"  && color === "blue" && "red") {
     $("#servo").show();
     }
-   else if ((age >= 18 && age <=35 && gender === "female"  && color === "blue" && "red"  && "green") {
+   else if (age >=18 && gender === "female"  && color === "blue" && "red"  && "green") {
       $("#gypsy").show();
-    // alert("test");
-}
+    }
+    else if (age >=18 /*&& age <=100*/ && gender === "robot"  /*&& color === "blue" && "red"  && "green"*/) {
+      $("#crow").show();
+    }
+    else if (age <18) {
+      $("#no-robots").show();
+    }
+
+
   event.preventDefault();
   });
 
 });
+
+//the if branch can be front end as a function to call in back end
